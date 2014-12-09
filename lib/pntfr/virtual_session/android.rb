5bafbfa1577a83790843f45a94a1777d035366e8
tramuntanal <tramuntanal@gmail.com>
2014-12-09 22:52:16 +0100

    [FIX] When sending through GCM registered_ids is an array.

--- lib/pntfr/virtual_session/android.rb
+++ lib/pntfr/virtual_session/android.rb
@@ -1,14 +1,14 @@
 #
 # VirtualSession implementation for Android
 #
-
+require 'gcm'
 module Pntfr
   module VirtualSession
     class Android < Pntfr::VirtualSession::Base
       def initialize session
         super
         @notification_key= Pntfr.config.gcm[:notification_key]
-        @gcm= GCM.new(@notification_key) unless Pntfr.test_env?
+        @gcm= ::GCM.new(@notification_key) unless Pntfr.test_env?
       end
 
       def msg content
@@ -23,6 +23,7 @@ module Pntfr
         if Pntfr.test_env?
           Pntfr.add_delivery(@push_id, options)
         else
+          puts "PUSH_ID: #{@push_id}, OPTIONS: #{options}"
           @gcm.send_notification(@push_id, options)
         end
       end
