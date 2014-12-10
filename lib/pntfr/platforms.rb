56f7e3c035f7113e446a4963b626968cc76e31e3
tramuntanal <tramuntanal@gmail.com>
2014-12-10 17:32:05 +0100

    Added static methods to Platforms for better usage.

--- lib/pntfr/platforms.rb
+++ lib/pntfr/platforms.rb
@@ -10,5 +10,20 @@ module Pntfr
       return false if string.nil? or string.empty?
       IOS == string.upcase
     end
+    # All supported platforms
+    def self.all
+      [ANDROID, IOS]
+    end
+    # Returns a list of platforms:
+    # - if the +platform+ param is present wraps it into an Array.
+    # - else returns all supported platforms.
+    #
+    def self.given_or_default_to_all(platform)
+      if platform
+        [platform]
+      else
+        Pntfr::Platforms.all
+      end
+    end
   end
 end
