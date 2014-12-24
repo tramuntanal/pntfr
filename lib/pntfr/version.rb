b9f9552b69dd85caf1c8269d77f5fd2461830294
tramuntanal <tramuntanal@gmail.com>
2014-12-24 12:03:14 +0100

    Version 0.1.5

--- lib/pntfr/version.rb
+++ lib/pntfr/version.rb
@@ -1,6 +1,4 @@
 module Pntfr
-  # [FEATURE] Accept custom content.
-  # [DOC] Documentation for testing.
-  # [TEST] Facilitate Rails testing.
-  VERSION = '0.1.4'
+  # [FIX] Custom content is a "others" key in the content argument to APNS.
+  VERSION = '0.1.5'
 end
