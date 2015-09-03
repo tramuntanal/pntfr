38a85769ef27b04a400d8312f97b642b14f7bdc2
tramuntanal <tramuntanal@gmail.com>
2015-09-03 14:46:03 +0200

    [DOC] Changelog.

--- lib/pntfr/version.rb
+++ lib/pntfr/version.rb
@@ -1,12 +1,13 @@
 module Pntfr
   #
+  # PATCH v0.3.1
+  # - [FEATURE] When overriding ios credentials, merge over general configuration.
+  #
+  # MINOR v0.3.0
   # - Performance improvement: Allow sending one message to many devices in one
   # single call (on both platforms).
   # - Allow overriding general configuration credentials when instantiating each
   # Notifier (on both platforms).
   # - Internal refactoring.
-  #
-  # As this change don't break the gem's API (it extends it), lets change only
-  # minor version.
-  VERSION = '0.3.0'
+  VERSION = '0.3.1'
 end
